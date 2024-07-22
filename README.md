ef7ef032b8541b4ecdbacdf6f4e983f077ed3f10
subhro-acharjee-tcgls <subhro.acharjee@tcgre.com>
2024-07-22 16:16:25 +0530

    chore/adding better docs

--- README.md
+++ README.md
@@ -18,7 +18,112 @@ and could also be integrated successfully with a variety of observability tools.
 npm i tcgre-logging
 ```
 
-### Usage
+## Features
+
+### File Transports
+
+There are two file transports, one is `combined` and another is `separated`.
+
+#### Combined
+
+When all the logs are in same file called `combined.log`
+
+```typescript
+const logProvider = new LoggerBuilder()
+  .setCallerPrefix(true)
+  .addCombinedFileLogs("<dirName>")
+  .build();
+```
+
+#### Separated
+
+When all the logs are separated by their levels.
+
+```typescript
+const logProvider = new LoggerBuilder()
+  .setCallerPrefix(true)
+  .addSeparatedFileLogs("<dirName>")
+  .build();
+```
+
+### Transports
+
+By default it accepts the winstons transports as we are using winston under the
+hood. So a usecase where we need to add transport for lets say grafana then we
+will be using [winston-loki](https://www.npmjs.com/package/winston-loki), by
+setting it up during application bootstrap and passing to addTransports.
+
+```typescript
+...
+ const logProvider = new LoggerBuilder()
+    .setCallerPrefix(true)
+    .addTransports(
+      new winston.transports.Http(),
+      new winston.transports.LokiTransport(),
+      new winston.transports.DbTransport(),
+    )
+    .build();
+...
+```
+
+### Formats
+
+We are using [LogForm](https://www.npmjs.com/package/logform) for formatting
+our logs. By default, `json`, `errors`, `timestamp` is already setted up, in
+case, if you want more please refer to the docs for logform.
+
+```typescript
+import {format} from 'logform'
+...
+const volumeFormat = format((info, opts) => {
+  if (opts.yell) {
+    info.message = info.message.toUpperCase();
+  } else if (opts.whisper) {
+    info.message = info.message.toLowerCase();
+  }
+
+  return info;
+});
+...
+ const logProvider = new LoggerBuilder()
+    .setCallerPrefix(true)
+    .addFormats(
+      format.cli(),
+      volume(),
+    )
+    .build();
+...
+```
+
+### LogLevels
+
+So by default the log levels are as such.
+
+```typescript
+const defaultLogLevels = {
+  fatal: 0,
+  error: 1,
+  warn: 2,
+  info: 3,
+  debug: 4,
+};
+```
+
+And default Level is set to `debug`. Change it using.
+
+```typescript
+...
+const loggerProvider = new LoggerBuilder().setLogLevel("info").build();
+...
+```
+
+### Silencing Console Logs
+
+use the env variable to silence the console logs.
+
+```bash
+export LOG_SILENT=true;
+```
 
 ### Attach request ids to request scope
 
--- src/lib/log-builder.ts
+++ src/lib/log-builder.ts
@@ -3,13 +3,20 @@ import "winston-daily-rotate-file";
 import { LogsProviderInterface } from "../interfaces/logs-provider.interface";
 import { LogsProvider } from "./logger";
 import { LogLevels } from "../types/log-level";
+import { LogType } from "../../dist";
 
+/**
+ * A builder class for LogsProvider
+ * @class
+ * @public
+ */
 export class LoggerBuilder {
   private timestampFormat: string;
   private addCallerNameToMessageAsPrefix: boolean;
   private defaultFormats: winston.Logform.Format[];
   private defaultTransport: winston.transport[];
   private defaultLogLevels: LogLevels;
+  private defaultLogLevel: LogType;
   private separatedFileMeta?: {
     dirname: string;
     rotate?: string;
@@ -27,32 +34,73 @@ export class LoggerBuilder {
       fatal: 0,
       error: 1,
       warn: 2,
-      debug: 3,
-      info: 4,
+      info: 3,
+      debug: 4,
     };
+    this.defaultLogLevel = "debug";
   }
 
-  setCallerPrefix(value?: boolean) {
+  /**
+   * Sets addCallerNameToMessageAsPrefix property.
+   * @param {boolean} [value] - New value.
+   * @returns {LoggerBuilder} LoggerBuilder instance.
+   */
+  setCallerPrefix(value?: boolean): LoggerBuilder {
     this.addCallerNameToMessageAsPrefix = value ?? true;
     return this;
   }
 
-  setTimestampFormat(format: string) {
+  /**
+   * Sets the format of the timestamp for log messages
+   * @param {string} format - The timestamp format.
+   * @returns {LoggerBuilder} LoggerBuilder instance.
+   */
+  setTimestampFormat(format: string): LoggerBuilder {
     this.timestampFormat = format;
     return this;
   }
 
-  addFormats(...formats: winston.Logform.Format[]) {
+  /**
+   * Adds additional format(s) for the log
+   * @param {...winston.Logform.Format[]} formats - The format(s) to add
+   * @returns {LoggerBuilder} LoggerBuilder instance.
+   */
+  addFormats(...formats: winston.Logform.Format[]): LoggerBuilder {
     this.defaultFormats.push(...formats);
     return this;
   }
 
-  addTransports(...transports: winston.transport[]) {
+  /**
+   * Adds transport(s) for the log
+   * @param {...winston.transport[]} transports - The transport(s) to add
+   * @returns {LoggerBuilder} LoggerBuilder instance.
+   */
+  addTransports(...transports: winston.transport[]): LoggerBuilder {
     this.defaultTransport.push(...transports);
     return this;
   }
 
-  addCombinedFileLogs(dirname: string = ".logs", rotate?: string) {
+  /**
+   * Sets the default log level for the log. Default value is set to debug. If
+   * set to `info` then no debug logs will be visible.
+   * @param {LogType} level - The log level
+   * @returns {LoggerBuilder} LoggerBuilder instance.
+   */
+  setLogLevel(level: LogType): LoggerBuilder {
+    this.defaultLogLevel = level;
+    return this;
+  }
+
+  /**
+   * Adds a single file for all the logs called combined.log.
+   * @param {string} [dirname=".logs"] - The directory name
+   * @param {string} [rotate] - The rotation strategy
+   * @returns {LoggerBuilder} LoggerBuilder instance.
+   */
+  addCombinedFileLogs(
+    dirname: string = ".logs",
+    rotate?: string
+  ): LoggerBuilder {
     if (rotate && typeof rotate === "string" && rotate.length > 0) {
       this.defaultTransport.push(
         new transports.DailyRotateFile({
@@ -71,7 +119,16 @@ export class LoggerBuilder {
     return this;
   }
 
-  addSeparatedFileLogs(dirname: string = ".logs", rotate?: string) {
+  /**
+   * Sets up separated logging for different levels in provided directory
+   * @param {string} [dirname=".logs"] - The directory name
+   * @param {string} [rotate] - The rotation strategy
+   * @returns {LoggerBuilder} LoggerBuilder instance.
+   */
+  addSeparatedFileLogs(
+    dirname: string = ".logs",
+    rotate?: string
+  ): LoggerBuilder {
     this.separatedFileMeta = {
       dirname: dirname,
       rotate,
@@ -102,6 +159,10 @@ export class LoggerBuilder {
     }
   }
 
+  /**
+   * Builds and returns a log provider
+   * @returns {LogsProviderInterface} - The log provider.
+   */
   build(): LogsProviderInterface {
     this.defaultFormats.unshift(
       format.timestamp({ format: this.timestampFormat }),
@@ -116,6 +177,7 @@ export class LoggerBuilder {
       transports: this.defaultTransport,
       format: format.combine(...this.defaultFormats),
       levels: this.defaultLogLevels,
+      level: this.defaultLogLevel,
     };
 
     LogsProvider.getInstance(config);
